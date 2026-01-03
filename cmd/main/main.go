cf679380ec81df2ac7d3faa01bae5127565c6a8c
SteliosSpanos <stylspan19@gmail.com>
2026-01-03 14:08:28 +0200

    Adds the main function

---
+++ cmd/main/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/SteliosSpanos/mini-CAS/commands"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ./cas <command> <arguments>")
+		fmt.Println("    init    Initialize CAS")
+		fmt.Println("    add     Add file or directory in the storage")
+		fmt.Println("    ls      List all the contents")
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+	args := os.Args[2:]
+
+	switch command {
+	case "init":
+		commands.Init()
+	case "add":
+		commands.Add(args)
+	case "ls":
+		commands.List()
+	default:
+		fmt.Println("Not a valid command")
+		os.Exit(1)
+	}
+}
