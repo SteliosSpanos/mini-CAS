5ec1bc2a6d0700eae9327c6499489e440c24ddbd
SteliosSpanos <stylspan19@gmail.com>
2026-01-09 23:32:39 +0200

    Creates WriteBlobStream function

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,7 +1,9 @@
 package storage
 
 import (
+	"crypto/sha256"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 
@@ -49,4 +51,46 @@ func LoadBlob(casDir, hash string) (*objects.Blob, error) {
 
 	blob := objects.NewBlob(data)
 	return blob, nil
+}
+
+func WriteBlobStream(casDir string, reader io.Reader) (string, error) {
+	tmpFile, err := os.CreateTemp(filepath.Join(casDir, "storage"), "tmp-")
+	if err != nil {
+		return "", fmt.Errorf("failed to create temp file: %w", err)
+	}
+
+	tmpPath := tmpFile.Name()
+	defer os.Remove(tmpPath)
+	defer tmpFile.Close()
+
+	hasher := sha256.New()
+	multiWriter := io.MultiWriter(tmpFile, hasher)
+
+	if _, err := io.Copy(multiWriter, reader); err != nil {
+		return "", fmt.Errorf("failed to copy: %w", err)
+	}
+
+	hash := fmt.Sprintf("%x", hasher.Sum(nil))
+	tmpFile.Close()
+
+	objectDir := filepath.Join(casDir, "storage", hash[:2], hash[2:4])
+	objectPath := filepath.Join(objectDir, hash)
+
+	if _, err := os.Stat(objectPath); err == nil {
+		return hash, nil
+	}
+
+	if err := os.MkdirAll(objectDir, 0755); err != nil {
+		return "", fmt.Errorf("failed to create directory: %w", err)
+	}
+
+	if err := os.Rename(tmpPath, objectPath); err != nil {
+		return "", fmt.Errorf("failed to move file: %w", err)
+	}
+
+	if err := os.Chmod(objectPath, 0444); err != nil {
+		return "", fmt.Errorf("failed to set permissions: %w", err)
+	}
+
+	return hash, nil
 }
